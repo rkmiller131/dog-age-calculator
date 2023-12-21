b7017b1fb00cd42362809e7e66c8c833d2d7aae6
Rachel Miller <rkmiller131@gmail.com>
2023-12-21 14:12:17 -0800

    Fixed classname bug for dynamic h/w sizes for social icons

--- src/app/footer/LinkIcon.tsx
+++ src/app/footer/LinkIcon.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import classnames from 'classnames'
 import Image from 'next/image'
 
 type LinkIconProps = {
@@ -10,10 +11,16 @@ type LinkIconProps = {
 }
 
 const LinkIcon: React.FC<LinkIconProps> = ({ href, src, width, height, alt }) => {
-  const size = `w-[${width + 6}px] h-[${height + 6}px]`;
+  // dynamic class names with a default, and dynamic width + 6 that will be incorporated if width is truthy
+  const sizeClasses = classnames(
+    'w-[34px] h-[34px]', {
+      [`w-${width + 6}`]: width,
+      [`h-${height + 6}`]: height,
+    }
+  )
   return (
     <a href={href} target="_blank" rel="noopener noreferrer">
-      <div className={`relative flex items-center justify-center rounded m-[0.5rem] ${size} hover:bg-bubble active:bg-bubble`}>
+      <div className={`relative flex items-center justify-center rounded m-[0.5rem] ${sizeClasses} hover:bg-bubble active:bg-bubble`}>
         <Image
           src={src}
           alt={alt}
